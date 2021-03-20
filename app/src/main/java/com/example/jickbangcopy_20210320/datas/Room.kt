32616e1bc672f58633aaf5c17f5c8b44897711fd
NePPPlus_010 <kmc901206@gmail.com>
2021-03-20 15:20:46 +0900

    함수내부에 return 일일이 잘 적어주기

--- app/src/main/java/com/example/jickbangcopy_20210320/datas/Room.kt
+++ app/src/main/java/com/example/jickbangcopy_20210320/datas/Room.kt
@@ -1,21 +1,20 @@
 package com.example.jickbangcopy_20210320.datas
 
-class Room (
-    val price : Int,
-    val address : String,
-    val floor : Int,
-    val description : String) {
+class Room(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String
+) {
 
-    fun getFormattedFloor() : String {
+    fun getFormattedFloor(): String {
 
         if (this.floor >= 1) {
             return "${this.floor}층"
-        }
-        else if (this.floor == 0) {
+        } else if (this.floor == 0) {
             return "반지하"
-        }
-        else {
-            "지하${-this.floor}층"
+        } else {
+            return "지하${-this.floor}층"
         }
 
     }
